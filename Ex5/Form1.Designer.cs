0ad9f3b1e7c97c2316eab64a65a2df105ce0fe97
vhxubo <vhxubo@gmail.com>
2020-05-19 15:13:32 +0800

    feat: Ex5 is done

--- Ex5/Form1.Designer.cs
+++ Ex5/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_guess = new System.Windows.Forms.TextBox();
+            this.lb_result = new System.Windows.Forms.Label();
+            this.btn_guess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_guess
+            // 
+            this.tb_guess.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_guess.Location = new System.Drawing.Point(54, 71);
+            this.tb_guess.Name = "tb_guess";
+            this.tb_guess.Size = new System.Drawing.Size(234, 34);
+            this.tb_guess.TabIndex = 0;
+            // 
+            // lb_result
+            // 
+            this.lb_result.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb_result.Location = new System.Drawing.Point(12, 19);
+            this.lb_result.Name = "lb_result";
+            this.lb_result.Size = new System.Drawing.Size(318, 36);
+            this.lb_result.TabIndex = 1;
+            this.lb_result.Text = "请输入一个[0,100]的整数";
+            this.lb_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_guess
+            // 
+            this.btn_guess.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_guess.Location = new System.Drawing.Point(101, 132);
+            this.btn_guess.Name = "btn_guess";
+            this.btn_guess.Size = new System.Drawing.Size(131, 56);
+            this.btn_guess.TabIndex = 2;
+            this.btn_guess.Text = "Guess";
+            this.btn_guess.UseVisualStyleBackColor = true;
+            this.btn_guess.Click += new System.EventHandler(this.btn_guess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(342, 213);
+            this.Controls.Add(this.btn_guess);
+            this.Controls.Add(this.lb_result);
+            this.Controls.Add(this.tb_guess);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "随机数小游戏";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_guess;
+        private System.Windows.Forms.Label lb_result;
+        private System.Windows.Forms.Button btn_guess;
     }
 }
 
--- Ex5/Form1.cs
+++ Ex5/Form1.cs
@@ -1,20 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Ex5
 {
     public partial class Form1 : Form
     {
+        private int answer = new Random().Next(0, 101);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_guess_Click(object sender, EventArgs e)
+        {
+            if (tb_guess.Text == "")
+            {
+                MessageBox.Show(this, "请输入 [0,100] 的整数!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            int result = Convert.ToInt32(tb_guess.Text);
+            if (result < 0 || result > 100)
+            {
+                MessageBox.Show(this, "请输入 [0,100] 的整数!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (result == answer)
+            {
+                MessageBox.Show(this, "恭喜你猜对了", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else if (result < answer)
+            {
+                lb_result.Text = "猜小了";
+            }
+            else
+            {
+                lb_result.Text = "猜大了";
+            }
         }
     }
 }
