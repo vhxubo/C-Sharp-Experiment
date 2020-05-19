6c2798eea86e6bb1073da28eebeab6d1141a1a2c
vhxubo <vhxubo@gmail.com>
2020-05-19 12:11:52 +0800

    feat: Ex2 is done

---
+++ Ex2/ArraySort.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex2
+{
+    class ArraySort
+    {
+        public void Sort(ref int[] vs)
+        {
+            int n = vs.Length - 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (vs[j + 1] < vs[j])
+                    {
+                        int temp = vs[j];
+                        vs[j] = vs[j + 1];
+                        vs[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
  编写一个排序类，实现如下功能：
@@ -17,6 +13,37 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("请在下方输入需要排序的数组，格式为：1,2,6,3,5,4 \n请使用英文符号！");
+
+            // 处理输入的数组
+            ArraySort arrSort = new ArraySort();
+            string temp = Console.ReadLine();
+            Array arr = temp.Split(',');
+
+            int[] arrNum = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrNum[i] = Convert.ToInt32(arr.GetValue(i));
+            }
+
+            // 数组排序
+            arrSort.Sort(ref arrNum);
+
+            // 格式化输出排序结果
+            Console.Write("Output: ");
+            for (int j = 0; j < arrNum.Length; j++)
+            {
+                Console.Write(arrNum[j]);
+                if (j != arrNum.Length - 1)
+                {
+                    Console.Write(",");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
